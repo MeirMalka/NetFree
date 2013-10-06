47897b9c0dcdfd26d4cfa15f33113d09fd590a2c
magicode <magicode7@gmail.com>
2013-10-06 11:38:45 +0200

    brand free

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,14 @@
 
 
-var phttp = require(__dirname + "/../lib/server");
+var free = require(__dirname + "/../lib/free");
 var http = require("http");
 
 var onnex = require("onnex").create();
 
 onnex.addConnect({ port: 5432 ,host: "db0.netfree.613m.org" , alwaysConnect: true });
 
-var s = phttp.createServer({ onnex: onnex });
+var httpFree = free.httpFree({ onnex: onnex });
 
-s.listen( 7777 ,"0.0.0.0" ,function(){
+httpFree.listen( 7777 ,"0.0.0.0" ,function(){
 	 console.log("run server on port 7777");
 }); 
